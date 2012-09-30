7c418b91a631720bc3832e0d56c17b631903d266
topac <topac@users.noreply.github.com>
2012-09-30 13:28:58 +0200

    not used

--- public/coffeescripts/config/sprites.config.js
+++
@@ -1,5 +0,0 @@
-fi.config.sprites =
-  alien1:  {url: 'images/sprites.png', sx: 0,  sy: 0,  w: 23, h: 18, cls: Alien, frames: 2}
-  alien2:  {url: 'images/sprites.png', sx: 0,  sy: 18, w: 23, h: 18, cls: Alien, frames: 2}
-  player:  {url: 'images/sprites.png', sx: 0,  sy: 36, w: 26, h: 17, cls: Player}
-  missile: {url: 'images/sprites.png', sx: 0,  sy: 86, w: 3,  h: 14, cls: Missile}
